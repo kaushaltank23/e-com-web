a51e445f7957eb4f8dcf2044888ba91c7c456751
kaushaltank23 <kevitank2302@gmail.com>
2024-04-18 18:44:07 +0530

    task

---
+++ e-com/index.js
@@ -0,0 +1,109 @@
+// Get allValues using querySelectorAll
+let allValues = document.querySelectorAll(".value");
+
+// Start the forEach loop for displaying the values
+allValues.forEach((singleValue) => {
+  let startValue = 0;
+  let endValue = parseInt(singleValue.getAttribute("data-value"));
+  let duration = Math.floor(2000 / endValue);
+
+  // Counter for increaing the values & display
+  let counter = setInterval(function () {
+    startValue += 1;
+    singleValue.textContent = startValue;
+    // Clearing the interval
+    if (startValue == endValue) {
+      clearInterval(counter);
+    }
+  }, duration);
+});
+
+// slider
+
+const slider = document.querySelector(".slider");
+
+function activate(e) {
+  const items = document.querySelectorAll(".item");
+  e.target.matches(".next") && slider.append(items[0]);
+  e.target.matches(".prev") && slider.prepend(items[items.length - 1]);
+}
+
+document.addEventListener("click", activate, false);
+
+// review
+
+const testimonials = [
+  {
+    name: "Cherise G",
+    photoUrl:
+      "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=880&q=80",
+    text: "This is simply unbelievable! I would be lost without Apple. The very best. Not able to tell you how happy I am with Apple.",
+  },
+  {
+    name: "Rosetta Q",
+    photoUrl:
+      "https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=880&q=80",
+    text: "I would also like to say thank you to all your staff. Wow what great service, I love it! Apple impressed me on multiple levels.",
+  },
+  {
+    name: "Constantine V",
+    photoUrl:
+      "https://images.unsplash.com/photo-1628157588553-5eeea00af15c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=880&q=80",
+    text: "Thank you for making it painless, pleasant and most of all hassle free! I wish I would have thought of it first. The very best.",
+  },
+];
+
+const imgEl = document.querySelector(".review img");
+const textEl = document.querySelector(".text");
+const usernameEl = document.querySelector(".username");
+
+let idx = 0;
+
+updateTestimonial();
+
+function updateTestimonial() {
+  const { name, photoUrl, text } = testimonials[idx];
+  imgEl.src = photoUrl;
+  textEl.innerText = text;
+  usernameEl.innerText = name;
+  idx++;
+  if (idx === testimonials.length) {
+    idx = 0;
+  }
+  setTimeout(() => {
+    updateTestimonial();
+  }, 1200);
+}
+
+// registar
+
+const popup = document.querySelector(".pop-up");
+const buttonX = document.querySelector(".close-x");
+const screen = document.querySelector(".overlay");
+
+function showPopUp() {
+  popup.classList.toggle("active");
+  buttonX.classList.toggle("active");
+  screen.classList.toggle("active");
+}
+
+function closePopUp() {
+  popup.classList.remove("active");
+  buttonX.classList.remove("active");
+  screen.classList.remove("active");
+}
+
+buttonX.addEventListener("click", () => {
+  popup.classList.remove("active");
+  buttonX.classList.remove("active");
+  screen.classList.remove("active");
+});
+document.addEventListener("click", (event) => {
+  if (!popup.contains(event.target) && screen.contains(event.target)) {
+    popup.classList.remove("active");
+    buttonX.classList.remove("active");
+    screen.classList.remove("active");
+  }
+});
+
+// google
